3336a12c605f7dd65e7615d03b2ea57bf288a341
docrea <rea@docrea.net>
2018-11-06 11:34:09 -0500

    Exercise1.java
    Make a copy of this in your branch, then code and test it.
    
    Commit back to your branch online, but no need for a pull request to master.

---
+++ Exercise1.java
@@ -0,0 +1,38 @@
+/*************************************
+* Programmer: Your Name
+* Exercise1: Hello World
+* Irish Robotics
+* Fall 2018
+* Date completed: 11/??/18
+**************************************
+* Program Explanation
+* 
+* Write about what your program does, how it does it, and why.
+* 
+**************************************/
+
+//Explain what this line does (then delete this comment).
+package exercise1;
+
+/**
+ *
+ * @author Your Name
+ */
+
+    //Explain what this line does (then delete this comment).
+public class Exercise1 {
+
+    //Explain what this line does (then delete this comment).
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+        // Declare a variable to hold your name
+        // Declare a variable to hold your age
+        // Output your name
+        // Output your age
+        // Make sure to explaine everything about your code.
+        
+        
+    }
+    
+}
