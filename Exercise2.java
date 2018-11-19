25dcafd0ceaf5c0aae04cfb9d8c4bec40c6968fd
docrea <rea@docrea.net>
2018-11-19 16:45:19 -0500

    Exercise2 Online
    Clone Exercise2 in a branch.
    
    When complete, please push it back to your branch and docrea will check it.

---
+++ Exercise2.java
@@ -0,0 +1,50 @@
+/*************************************
+* Programmer: Your Name
+* Exercise2: IPO
+* Irish Robotics
+* Fall 2018
+* Date completed: 11/??/18
+**************************************
+* Program Explanation
+* 
+* Write about what your program does, how it does it, and why.
+* 
+**************************************/
+
+
+/**
+ *
+ * @author Your Name
+ */
+
+    //Explain what this line does (then delete this comment).
+public class Exercise2 {
+
+    //Explain what this line does (then delete this comment).
+    public static void main(String[] args) 
+    {
+        /* Review the JavaBook Wiki page at
+
+        https://github.com/reateach/JavaBook/wiki/Strings
+
+        Then, do the following:
+
+        1. Get a user's name
+        2. Get an integer (intOne)
+        3. Get another integer (intTwo)
+        4. Calculate the sum, difference, product, and quotient. Store them in 
+            variabled.
+        5. Output the user's name
+        6. Output the sum of the numbers
+        7. Output the difference of the numbers
+        8. Output the product of the numbers
+        9. Output the quotient of the numbers (anything off?)
+
+        */
+
+        // Make sure to explaine everything about your code.
+        
+        
+    }
+    
+}
